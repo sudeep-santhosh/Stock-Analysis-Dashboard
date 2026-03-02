00c9350f393e9a2c0ceabe2371edc3cd04ed469a
sudeepisstupid123 <sudeepsant10@gmail.com>
2026-03-03 03:06:27 +0530

    indicator file having sma and partially written RSI

---
+++ indicators.py
@@ -0,0 +1,17 @@
+def add_sma(df):
+    """
+    Adds 50 and 200 SMA columns to dataframe.
+    """
+
+    df["SMA50"] = df["Close"].rolling(window=50).mean()
+    df["SMA200"] = df["Close"].rolling(window=200).mean()
+
+    return df
+
+def add_rsi(df,period=14):
+    delta=df["Close"].diff()
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+    avg_gain = gain.ewm(alpha=1/period, adjust=False).mean()
+    avg_loss = loss.ewm(alpha=1/period, adjust=False).mean()
+    
