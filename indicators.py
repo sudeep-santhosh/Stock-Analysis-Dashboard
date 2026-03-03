dda28caf743efec9fa71d6be742ebd4f612376b5
sudeepisstupid123 <sudeepsant10@gmail.com>
2026-03-03 17:18:07 +0530

    completed RSi indicator and working on another indicator

--- indicators.py
+++ indicators.py
@@ -14,4 +14,6 @@ def add_rsi(df,period=14):
     loss = -delta.clip(upper=0)
     avg_gain = gain.ewm(alpha=1/period, adjust=False).mean()
     avg_loss = loss.ewm(alpha=1/period, adjust=False).mean()
-    
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
